a677b7a48eda68ccfb318b1a13326c4548830870
Mars Kim <marsinearth@gmail.com>
2023-03-09 01:27:08 +0900

    add: slack message via incoming webhook for the lambda functiuon response
    refactor: add mode='test' for test code to process the crawling for only one detail page to reduce test time

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,9 +1,11 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import fastify from 'fastify';
+import qs from 'qs';
 import app from './app';
 
 const server = fastify({
   logger: true,
+  querystringParser: (str) => qs.parse(str),
 });
 
 server.register(app);
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify';
+import qs from 'qs';
 import app from './app';
 
 const server = fastify({
   logger: true,
+  querystringParser: (str) => qs.parse(str),
 });
 
 server.register(app);
